8481aeed8e86ad25d17e653f99874213365a7189
Joseph_M <citytrotkenya@gmail.com>
2021-04-12 12:27:31 +0300

    init commit

---
+++ dre.pt_scraper_.py
@@ -0,0 +1,76 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+
+
+
+def item_dict (a_listing):
+    listing_dict = {}
+    
+    listing_dict['Url'] = a_listing.a['href']
+    listing_dict['Title'] = a_listing.a.text.strip()
+
+    author= a_listing.find('div',{"class":"author"})
+    listing_dict['Author'] = author.text.strip()
+
+    summary= a_listing.find('div',{"class":"summary"})
+    listing_dict['Summary'] = summary.text.strip()
+    
+    return listing_dict
+
+
+
+
+headers = {
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-User': '?1',
+    'Sec-Fetch-Dest': 'document',
+    'Referer': 'https://dre.pt/web/guest/home/-/dre/calendar/normal/II',
+    'Accept-Language': 'en-US,en;q=0.9',
+}
+
+
+master_listing = []
+
+for i in range(1,5):
+    #print('Getting page' + str(i))
+
+    params = (
+        
+        ('serie', 'II^'),
+        ('at', 'c^'),
+        ('parte_filter', '41'),
+        ('number', str(i)),
+    )
+
+    response = requests.get('https://dre.pt/web/guest/home/-/dre/144452178/details/maximized', headers=headers, params=params )
+
+    #NB. Original query string below. It seems impossible to parse and
+    #reproduce query strings 100% accurately so the one below is given
+    #in case the reproduced version is not "correct".
+    # response = requests.get('https://dre.pt/web/guest/home/-/dre/144452178/details/maximized?serie=II^&at=c^&parte_filter=41', headers=headers, cookies=cookies)
+
+    html = response.text
+    soup = BeautifulSoup(html,'html.parser')
+
+
+    listing = soup.find_all("ul")[6]
+    post = listing.find_all("li")
+
+
+    for a_listing in post:
+        post = item_dict(a_listing)
+        master_listing.append(post)
+
+
+
+df = pd.DataFrame(master_listing)
+df.to_csv('dre_final_trial.csv',index = False)
+
---
+++ tweepzy-autoLogin-scraper.py
@@ -0,0 +1,100 @@
+#imports
+import selenium
+import csv
+from getpass import getpass
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchAttributeException
+# from selenium.webdriver import Chrome
+
+#function
+def get_tweet_data(card):
+    """Extract from tweet data"""
+    username = card.find_element_by_xpath('.//span').text
+    handle = card.find_element_by_xpath('.//span[contains(text(), "@")]').text
+    try:
+        postdate = card.find_element_by_xpath('.//time').get_attribute('datetime')
+    except NoSuchAttributeException:
+        return
+    comment = card.find_element_by_xpath('.//div[2]/div[2]/div[1]').text
+    responding = card.find_element_by_xpath('.//div[2]/div[2]/div[2]').text#.strip()
+    text = comment + responding
+    reply_ctn = card.find_element_by_xpath('.//div[@data-testid="reply"]').text
+    retweet_ctn = card.find_element_by_xpath('.//div[@data-testid="retweet"]').text
+    like_ctn = card.find_element_by_xpath('.//div[@data-testid="like"]').text
+    
+    tweet = (username, handle, postdate, text, reply_ctn, retweet_ctn, like_ctn)
+    return tweet
+
+#create instance of webdriver
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+#navigate to login screen
+driver.get('https://www.twitter.com/login')
+driver.maximize_window()
+
+username = driver.find_element_by_xpath('//input[@name="session[username_or_email]"]')
+username.send_keys('Harry72426254')
+
+# my_password = getpass()
+password = driver.find_element_by_xpath('//input[@name="session[password]"]')
+password.send_keys('30422403josmacmm')
+password.send_keys(Keys.RETURN)
+
+#find search input & search for term
+search_input = driver.find_element_by_xpath('//input[@aria-label="Search query"]')
+search_input.send_keys('@SmascoCare')
+search_input.send_keys(Keys.RETURN)
+
+#navigate to 'latest' tab
+# selenium.webdriver.support.wait.WebDriverWait()
+driver.implicitly_wait(60)
+driver.find_element_by_link_text("Latest").click()
+
+#get all tweets on the page
+
+data = []
+tweet_ids = set()
+last_position = driver.execute_script("return window.paggeYOffset;")
+scrolling = True
+
+while scrolling:
+    page_cards = driver.find_elements_by_xpath('//div[@data-testid="tweet"]')
+    for card in page_cards[-15:]:
+        tweet = get_tweet_data(card)
+        if tweet:
+            tweet_id = ''.join(tweet)
+            if tweet_id not in tweet_ids:
+                tweet_ids.add(tweet_id)
+                data.append(tweet)
+    
+    scroll_attempt = 0
+    while True:
+        #check scroll position
+        driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+        sleep(2)
+
+        curr_position = driver.execute_script("return window.paggeYOffset;")
+        if last_position == curr_position:
+            scroll_attempt += 1
+            
+            #end scroll region
+            if scroll_attempt >=5:
+                scrolling = False
+                break
+            
+            else:
+                sleep(6) #attempt to scroll again
+                
+        else:
+            last_position = curr_position
+            break
+
+with open('data_tweet.csv', 'w', newline ='', encoding = 'utf-8') as f:
+    header = ['Username', 'Handle', 'Timestamp', 'Comments', 'Likes', 'Retweets', 'Text']
+    writer = csv.writer(f)
+    writer.writerow(header)
+    writer.writerows(data)
